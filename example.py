116d668e5008f967613cc1b22eeda6d4de00090a
Sanjiv Raj <sanjiv.raj@gmail.com>
2020-02-27 13:27:28 +0530

    Update example to Python-3

--- example.py
+++ example.py
@@ -7,15 +7,15 @@ from sbi_rates import rate_history
 
 deposits = []
 
-deposits.append(Deposit(id="1", principal=300000.0, interval=4, 
-      rate=9.25, started="16/4/2011", duration_days=555, closed="4/11/2014"))
-deposits.append(Deposit(id="2", principal=400000.0, interval=4, 
-      rate=9.25, started="17/4/2011", duration_days=555, closed="4/11/2014"))
-deposits.append(Deposit(id="3", principal=750000.0, interval=4, 
-      rate=9.0, started="21/11/2014", duration=2))
-deposits.append(Deposit(id="4", principal=500000.0, interval=4, 
-      rate=9.0, started="20/11/2014", duration=2))
- 
+deposits.append(Deposit(id="715", principal=391889.0, interval=4,
+      rate=7.10, started="24/11/2018", duration=2))
+deposits.append(Deposit(id="701", principal=914338.0, interval=4,
+      rate=7.10, started="22/11/2018", duration=2))
+deposits.append(Deposit(id="699", principal=979611.0, interval=4,
+      rate=7.1, started="21/11/2018", duration=2))
+deposits.append(Deposit(id="692", principal=653049.0, interval=4,
+      rate=7.1, started="20/11/2018", duration=2))
+
 #Year to get tax statement for
 argparser = argparse.ArgumentParser()
 argparser.add_argument("--year", default=(datetime.now().timetuple()[0] - 1), type=int)
@@ -33,7 +33,4 @@ tax_edate = date(args.year,12,31)
 for deposit in all_deposits:
    deposit_tax_statement = deposit.tax_statement(tax_sdate, tax_edate)
    if deposit_tax_statement is not None:
-    print "{0}\t{1}".format(deposit, deposit_tax_statement)
-
-
-
+    print("{0}\t{1}".format(deposit, deposit_tax_statement))
